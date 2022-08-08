5bb3e7211a8e0679ff51fa8841702ea9eb4a73d8
Jair Santiago Leal Miranda <jair.leal@udea.edu.co>
2022-08-08 14:05:38 -0500

    funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  num1: number = 0;
-  num2: number = 0 ;
+  pantalla: string = "";
+  keydown(event:MouseEvent) {
+    let botonHtml:any= event.composedPath()[0];
+    let boton:string= botonHtml.id;
+    switch(boton){
+      case "botones":
+        return;
+      case "C":
+        this.pantalla="";
+        break;
+      case "←":
+        this.pantalla = this.pantalla.includes("Error")? "": this.pantalla;
+        this.pantalla = this.pantalla.substring(0, this.pantalla.length - 1);
+        break;
+      case "=":
+        this.pantalla = this.pantalla.includes("Error")? "": this.pantalla;
+        this.evaluar();
+        break;
+      default:
+        this.pantalla = this.pantalla.includes("Error")? "": this.pantalla;
+        this.pantalla = this.pantalla.length < 25? this.pantalla + boton: this.pantalla;
+        break;
+    }
+  }
+  evaluar(){
+    try{
+      let resultado:string = eval(this.pantalla).toString();
+      this.pantalla = resultado.length < 25? resultado: "Error";
+    }catch(e){
+      this.pantalla = "Error";
+    }
+  }
 }
